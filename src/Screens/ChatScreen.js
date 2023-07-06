a9aefb4d7e629feab4dd7995cbcd9e6449cd74cb
AyushiPatel261 <200556472@student.georgianc.on.ca>
2023-07-05 20:08:51 -0400

    added message screen

---
+++ src/Screens/ChatScreen.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components/native';
+import { Ionicons } from '@expo/vector-icons';
+import Header from '../Components/Header';
+import Chat from '../Components/MessageSection';
+import SearchComponent from '../Components/Search';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: white;
+`;
+
+ const ChatScreen = () => {
+  return (
+    <Container>
+      <Header headerTitle='Messages' IconName = "create-outline" />
+      <SearchComponent></SearchComponent>
+     <Chat name='Floyd Miles' message ="Okay, bye" />
+     <Chat name='Robert Fox'  message ="See you soon"/>
+     <Chat name='Cody Fisher' message ="Hey there"/>
+     <Chat name='Jacob Jones' message ="Yes, thanks!"/>
+
+  </Container>
+  )
+}
+
+export default ChatScreen
